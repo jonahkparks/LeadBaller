9d09e723faaacadddd760d61471ccbc0b7b012c6
Jeremy Lahners <jeremy@sitehands.com>
2022-06-14 12:27:23 -0400

    Added authentication function

--- LeadBaller.js
+++ LeadBaller.js
@@ -1,1 +1,28 @@
+var campaignsTest = 'br4n2s75h'; // Testing App - Campaigns table
+var campaignsProd = 'brx55z77r'; // Production App - Campaigns table
 
+function authenticateQuickBase(qbTable, prodInstance) {
+    var internalTable = '';
+
+    if (qbTable = 'campaigns') {
+        internalTable = (prodInstance = "yes" ? campaignsProd : campaignsTest );
+    }
+    var headers = {
+        'QB-Realm-Hostname': 'leadballer.quickbase.com',
+        'Content-Type': 'application/json'
+    };
+
+    var authToken = '';
+
+    $.ajax({
+        url: 'https://api.quickbase.com/v1/auth/temporary/' + internalTable,
+           method: 'GET',
+           async: false,
+        headers: headers,
+        xhrFields: { withCredentials: true },
+        success: function(result) {
+            authToken = result.temporaryAuthorization;
+        }
+    })
+
+}
