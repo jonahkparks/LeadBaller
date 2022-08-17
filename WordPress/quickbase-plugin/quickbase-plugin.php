b91406a8ae4aab0c2991558d6f6092a96ee1db0e
Jeremy Lahners <jeremy@sitehands.com>
2022-08-17 15:04:40 -0400

    Re-arranging files and adding WP plugin

---
+++ WordPress/quickbase-plugin/quickbase-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: LeadBaller QuickBase Plugin
+ * Description: Handles basic communication with QuickBase
+ * Version: 1.0.0
+ * Author: Jeremy Lahners
+ */
+
+ // Remove the admin bar from the front end
+ add_filter( 'show_admin_bar', '__return_false');
