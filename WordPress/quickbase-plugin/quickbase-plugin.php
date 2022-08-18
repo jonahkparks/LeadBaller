44c8b0c2176050a53aa3a7d1bf2bd14fa7e75ad2
Jeremy Lahners <jeremy@sitehands.com>
2022-08-18 09:50:47 -0400

    Completion of plugin foundation

--- WordPress/quickbase-plugin/quickbase-plugin.php
+++ WordPress/quickbase-plugin/quickbase-plugin.php
@@ -22,6 +22,15 @@
 
         // Add assets (js, css, etc)
         add_action('wp_enqueue_scripts', array($this, 'load_assets'));
+
+        // Add shortcode
+        add_shortcode( 'load-quickbase', 'load_shortcode' );
+
+        // Load javascript
+        add_action('wp_footer', array($this, 'load_scripts'));
+
+        // Register REST API
+        add_action('rest_api_init', array($this, 'register_rest_api'));
     }
 
     public function create_custom_post_type()
@@ -40,7 +49,7 @@
             'menu_icon' => 'dashicons-businessperson'
         );
 
-        register_post_type('quickbase_plugin', $args);
+        register_post_type('quickbase_plugin_form', $args);
     }
 
     public function load_assets() 
@@ -61,9 +70,66 @@
             false
         );
     }
+
+    public function load_shortcode()
+    {
+
+    }
+
+    public function load_scripts()
+    { ?>
+        <script>
+
+            var nonce = '<?php echo wp_create_nonce( 'wp_rest' );?>';
+
+            (function($) {
+                $('#quickbase-plugin__form').submit( function(event) {
+                    event.preventDefault();
+
+                    var form = $(this).serialize();
+
+                    $.ajax({
+                        method: 'post',
+                        url: '<?php echo get_rest_url(null, 'quickbase-plugin/v1/send-query');?>',
+                        headers: { 'X-WP-Nonce': nonce },
+                        data: form
+                    })
+                })
+            })
+        </script>
+    <?php }
+
+    public function register_rest_api()
+    {
+        register_rest_route( 'quickbase-plugin/v1', 'send-query', array(
+            'methods' => 'POST',
+            'callback' => array($this, 'handle_quickbase_query')
+        ));
+    }
+
+    public function handle_quickbase_query($data)
+    {
+        $headers = $data->get_headers();
+        $params = $data->get_params();
+
+        $nonce = $headers['x_wp_nonce'][0];
+
+        if(!wp_verify_nonce( $nonce, 'wp_rest' ))
+        {
+            return new WP_REST_Response('Invalid Details', 422);
+        }
+
+        $post_id = wp_insert_post([
+            'post_type' => 'quickbase_plugin_form',
+            'post_title' => 'Query',
+            'post_status' => 'publish'
+        ])
+
+        if($post_id) 
+        {
+            return new WP_REST_Response('Thank you', 200);
+        }
+    }
  }
 
  new QuickBasePlugin();
-
- // Remove the admin bar from the front end
- add_filter( 'show_admin_bar', '__return_false');
