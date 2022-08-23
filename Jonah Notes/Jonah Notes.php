e3e7e7ecc38f6da1c8896d8109e6d45adab90443
jonah <jonah@leadballer.com>
2022-08-23 13:51:42 -0400

    Made html and php notes

---
+++ Jonah Notes/Jonah Notes.php
@@ -0,0 +1,18 @@
+
+<?php
+
+$title = "This is my home page";
+$description = "This is my description";
+?>
+
+<html>
+    <head>
+        <title>Home Page</title>
+    </head>
+    <body>
+        <h1>
+            <?php echo $title; ?>
+        </h1>
+        <h2><?php print $description; ?></h2>
+    </body>
+</html>
