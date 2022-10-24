3071eddeb85971ebd604372e3546e6bab1d2f610
jonah <jonah@leadballer.com>
2022-10-24 14:10:19 -0400

    Second push

--- GoogleCloudScript.ps1
+++ GoogleCloudScript.ps1
@@ -1,11 +1,13 @@
-$files = Import-Csv -Path "C:\Users\Jonah\Desktop\GCloudList.csv"
+$files = Import-Csv "C:\Users\Jonah\Desktop\GCloudList.csv"
 
 #This command finds and replaces old video names with new video names
 foreach($file in $files) {
-    Rename-Item -Path {$file."Output Video Name" + ".mp4"} -NewName {$file."New Video ID" + ".mp4"}
+    Rename-Item {$file."Output Video Name" + ".mp4"} {$file."New Video ID" + ".mp4"}
 }
 
 #This command finds and replaces old image names with new image names
 foreach($file in $files) {
-    Rename-Item -Path {$file."Output Video Name" + ".png"} -NewName {$file."New Video ID" + ".png"}
+    Rename-Item {$file."Output Video Name" + ".png"} {$file."New Video ID" + ".png"}
 }
+
+echo $file.'Output Video Name' & ".png"
