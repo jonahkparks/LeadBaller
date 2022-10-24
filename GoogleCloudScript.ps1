ba0f89c5c3a5640c3fbf3d11122ac8dcc2863af4
jonah <jonah@leadballer.com>
2022-10-24 14:37:09 -0400

    Changed some things

--- GoogleCloudScript.ps1
+++ GoogleCloudScript.ps1
@@ -2,21 +2,28 @@ $files = Import-Csv "C:\Users\Jonah\Desktop\GCloudList.csv"
 
 #This command finds and replaces old video names with new video names
 foreach($file in $files) {
-    $oldFile = $file."Output Video Name"
+    $oldFile = $file.EmailUniqueID
     $oldFile += ".mp4"
 
     $newFile = $file."New Video ID"
     $newFile += ".mp4"
 
-    echo $oldFile
-    echo $newFile
+    # Write-Output $oldFile
+    # Write-Output $newFile
 
-    #Rename-Item {$file."Output Video Name" + ".mp4"} {$file."New Video ID" + ".mp4"}
+    Rename-Item -Path $oldFile -NewName $newFile
 }
 
 #This command finds and replaces old image names with new image names
 foreach($file in $files) {
-    Rename-Item {$file."Output Video Name" + ".png"} {$file."New Video ID" + ".png"}
-}
+    $oldFile = $file.EmailUniqueID
+    $oldFile += ".png"
 
-echo $file.'Output Video Name' + ".png"
+    $newFile = $file."New Video ID"
+    $newFile += ".png"
+
+    # Write-Output $oldFile
+    # Write-Output $newFile
+
+    Rename-Item -Path $oldFile -NewName $newFile
+}
